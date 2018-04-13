512492b3ef93eb344dd9c5298be757c5c6f896d7
lhy <zwkssdfz@163.com>
2018-04-13 11:26:23 +0800

    1 update

--- src/main/java/ServletTest.java
+++ src/main/java/ServletTest.java
@@ -9,7 +9,7 @@ public class ServletTest extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response){
 
         try {
-            response.getWriter().println("<h1>Hello Servlet!!!</h1>");
+            response.getWriter().println("<h1>Hello Servlet gitTest!!!</h1>");
             response.getWriter().println(new Date().toLocaleString());
         } catch (IOException e) {
             // TODO Auto-generated catch block
